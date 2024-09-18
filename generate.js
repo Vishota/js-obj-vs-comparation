2ac02e3eea186786df251a58ff95a5aa14594b40
Vishota <vishotax3@gmail.com>
2024-09-19 02:10:13 +0300

    a lot of selections

--- generate.js
+++ generate.js
@@ -1,15 +1,16 @@
 const fs = require('fs')
 
-const complexity = 1000
+const keysCount = 1000
+const complexity = 100000000
 
-const keys = new Array(complexity).fill(0).map(() => 'key' + Math.random().toString(36).slice(2))
+const keys = new Array(keysCount).fill(0).map(() => 'key' + Math.random().toString(36).slice(2))
 const target = keys[Math.floor(Math.random() * keys.length)]
 
-const makeObject = () => '{' + keys.map((key) => key + ': () => console.log(Math.random())').join(',') + '}'
-const makeIfelse = () => keys.reduce((prev, key) => prev + `if (target=='${key}') { console.log(Math.random()); } else `, '').slice(0, -5)
+const makeObject = () => '{' + keys.map((key) => key + ': () => Math.random()').join(',') + '}'
+const makeIfelse = () => keys.reduce((prev, key) => prev + `if (target=='${key}') { Math.random(); } else `, '').slice(0, -5)
 
-const objectJsCode = `const obj = ${ makeObject() }; const startTime = process.hrtime(); obj.${target}(); console.log(process.hrtime(startTime))`;
-const ifElseCode = `const target = '${target}'; const startTime = process.hrtime(); ${makeIfelse()}; console.log(process.hrtime(startTime))`;
+const objectJsCode = `const target = '${target}'; const obj = ${ makeObject() }; const startTime = process.hrtime(); for (let i = 0; i < ${complexity}; i++) obj.${target}(); console.log(process.hrtime(startTime))`;
+const ifElseCode = `const target = '${target}'; const startTime = process.hrtime(); for (let i = 0; i < ${complexity}; i++) ${ makeIfelse() }; console.log(process.hrtime(startTime))`;
 
 fs.writeFileSync('object.js', objectJsCode)
 fs.writeFileSync('ifelse.js', ifElseCode)
